b514a44f327f68fdb366be9fe5159c0af1eb1e9b
Maxim Kon0pelka <tkach32max@gmail.com>
2019-02-04 22:48:40 +0300

    Fix routes, fox example change radius to 1000

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    User.near([params[:lat], params[:lon]], 50, units: :km)
+    render json: User.near([params[:lat], params[:lon]], 1000, units: :km)
   end
 
   def show
-
+    render json: User.find_by_id(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :user, only: %i[index show]
+      resources :users, only: %i[index show]
     end
   end
 end
